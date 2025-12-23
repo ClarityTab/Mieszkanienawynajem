030cc76c825112de8397c964d8434d0a30c123d2
ClarityTab <callitaplan.planit@gmail.com>
2025-12-23 09:43:25 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,629 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-przedpokoj",
+      "name": "Przedpokój",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.27765292821188936,
+        "pitch": 0.2604516473411689,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1849145160912844,
+          "pitch": 0.1585748956262094,
+          "rotation": 0,
+          "target": "1-azienka"
+        },
+        {
+          "yaw": -0.594654630571684,
+          "pitch": 0.24187907917667673,
+          "rotation": 0,
+          "target": "4-salon"
+        },
+        {
+          "yaw": -0.21383685291990062,
+          "pitch": 0.4987030369412295,
+          "rotation": 0,
+          "target": "2-salon-wejscie"
+        },
+        {
+          "yaw": 0.23458155048066232,
+          "pitch": 0.177334591459946,
+          "rotation": 0,
+          "target": "5-salon-jadalnia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-azienka",
+      "name": "Łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.6090099651531311,
+        "pitch": 0.8607570836176031,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.782210986895528,
+          "pitch": 0.4144588462985155,
+          "rotation": 0,
+          "target": "0-przedpokoj"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-salon-wejscie",
+      "name": "Salon-wejście",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.0154003762398212,
+        "pitch": 0.36544423803453796,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8904575800661725,
+          "pitch": 0.26513612582125745,
+          "rotation": 0,
+          "target": "3-aneks-kuchenny"
+        },
+        {
+          "yaw": 0.7639318376483502,
+          "pitch": 0.31425445588707035,
+          "rotation": 0,
+          "target": "5-salon-jadalnia"
+        },
+        {
+          "yaw": -0.7205269922680344,
+          "pitch": 0.3771373885084479,
+          "rotation": 0,
+          "target": "4-salon"
+        },
+        {
+          "yaw": -1.798833586408712,
+          "pitch": 0.12248499850714012,
+          "rotation": 0,
+          "target": "8-schody"
+        },
+        {
+          "yaw": 3.0645601743256075,
+          "pitch": 0.22198934254783254,
+          "rotation": 0,
+          "target": "0-przedpokoj"
+        },
+        {
+          "yaw": -2.867428861369465,
+          "pitch": 0.25857391628224846,
+          "rotation": 0,
+          "target": "1-azienka"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-aneks-kuchenny",
+      "name": "Aneks kuchenny",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.0597061366305844,
+        "pitch": 0.7731702809141865,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2493220841513626,
+          "pitch": 0.2877327229771094,
+          "rotation": 0,
+          "target": "5-salon-jadalnia"
+        },
+        {
+          "yaw": -2.3287228934708715,
+          "pitch": 0.56557253824543,
+          "rotation": 0,
+          "target": "2-salon-wejscie"
+        },
+        {
+          "yaw": -2.1594495005334604,
+          "pitch": 0.20234875526001872,
+          "rotation": 0,
+          "target": "4-salon"
+        },
+        {
+          "yaw": -2.8114768076538823,
+          "pitch": 0.06726517256486808,
+          "rotation": 0,
+          "target": "8-schody"
+        },
+        {
+          "yaw": 2.992686984684318,
+          "pitch": 0.10242158844130778,
+          "rotation": 0,
+          "target": "1-azienka"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.5030364497737168,
+        "pitch": 0.7054658680228236,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5586123199455884,
+          "pitch": 0.2785732094756064,
+          "rotation": 0,
+          "target": "5-salon-jadalnia"
+        },
+        {
+          "yaw": 1.4441117266347492,
+          "pitch": 0.17418192324114656,
+          "rotation": 0,
+          "target": "3-aneks-kuchenny"
+        },
+        {
+          "yaw": 1.9240011846145872,
+          "pitch": 0.12101380225699288,
+          "rotation": 0,
+          "target": "0-przedpokoj"
+        },
+        {
+          "yaw": 2.5714151979916924,
+          "pitch": 0.011572352314459522,
+          "rotation": 0,
+          "target": "8-schody"
+        },
+        {
+          "yaw": 2.9412558321799853,
+          "pitch": -0.6504741931590985,
+          "rotation": 0,
+          "target": "7-antresola"
+        },
+        {
+          "yaw": 1.8294055395533562,
+          "pitch": 0.6368953552442811,
+          "rotation": 0,
+          "target": "2-salon-wejscie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-salon-jadalnia",
+      "name": "Salon-jadalnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.3587404026953998,
+        "pitch": 0.4305797161035514,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1330875524965816,
+          "pitch": 0.23408281961333977,
+          "rotation": 0,
+          "target": "4-salon"
+        },
+        {
+          "yaw": -0.27868750562855027,
+          "pitch": 0.271733230354565,
+          "rotation": 0,
+          "target": "6-balkon_1"
+        },
+        {
+          "yaw": 2.731693438125504,
+          "pitch": 0.3504331313282272,
+          "rotation": 0,
+          "target": "3-aneks-kuchenny"
+        },
+        {
+          "yaw": 3.104517056039989,
+          "pitch": 0.09809587677237808,
+          "rotation": 0,
+          "target": "0-przedpokoj"
+        },
+        {
+          "yaw": -2.9911166631285067,
+          "pitch": 0.18151458825898104,
+          "rotation": 0,
+          "target": "1-azienka"
+        },
+        {
+          "yaw": -2.6018878623318127,
+          "pitch": 0.05099650628565655,
+          "rotation": 0,
+          "target": "8-schody"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-balkon_1",
+      "name": "Balkon_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.43651021120459177,
+        "pitch": 0.7574708020679601,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7645355217747891,
+          "pitch": 0.09607461340274526,
+          "rotation": 0,
+          "target": "2-salon-wejscie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-antresola",
+      "name": "Antresola",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.5025621738622341,
+        "pitch": 0.2850181102570559,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6287771193599969,
+          "pitch": 0.27968426309216987,
+          "rotation": 0,
+          "target": "9-wc"
+        },
+        {
+          "yaw": 1.2450931586822733,
+          "pitch": 0.23721858213526303,
+          "rotation": 0,
+          "target": "10-garderoba"
+        },
+        {
+          "yaw": 2.6573077005856156,
+          "pitch": 0.8379033851468876,
+          "rotation": 0,
+          "target": "8-schody"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-schody",
+      "name": "Schody",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.0749011901171386,
+        "pitch": -0.11024400406472701,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7937412739976217,
+          "pitch": -0.5515586994865131,
+          "rotation": 0,
+          "target": "7-antresola"
+        },
+        {
+          "yaw": 1.3540676459162526,
+          "pitch": 0.4509756984300406,
+          "rotation": 0,
+          "target": "3-aneks-kuchenny"
+        },
+        {
+          "yaw": 0.9490280651117562,
+          "pitch": 0.7142771090950433,
+          "rotation": 0,
+          "target": "2-salon-wejscie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-wc",
+      "name": "WC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.4417534970222654,
+        "pitch": 0.6101575398498511,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.41852914867105184,
+          "pitch": 0.3588885033905367,
+          "rotation": 0,
+          "target": "7-antresola"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-garderoba",
+      "name": "Garderoba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.4307551875339968,
+        "pitch": 0.3051046337238823,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4632481798652854,
+          "pitch": 0.22193590700862842,
+          "rotation": 0,
+          "target": "7-antresola"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mieszkanie na wynajem",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
